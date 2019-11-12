262322336bdc5e05cd0379af3d86e4da91d4a5b0
jacob jolsö <jacke_j_1998@hotmail.se>
2019-11-12 15:57:23 +0100

    gjort så att man kan välja mellan 1 och två för ord och mening, mening ej klar

--- test hangman/Program.cs
+++ test hangman/Program.cs
@@ -24,65 +24,82 @@ namespace test_hangman
             //måste fixa så man kan gissa hela ord, och kanse så det inte tar upp mer än en liv, frågan är var i kode
             int lives = 10;
             Console.WriteLine(" your max guess: " + lives + "\n");
+            Console.WriteLine("click 1 to guess on word or click 2 to guess whole word and enter");
             bool won = false;
             int lettersRevealed = 0;
             // displaya liv mängd. fixa då så visa fel orden.
             string input;
             char guess;
-
-            while (!won && lives > 0)
+            var choose = Console.ReadLine();
+            if (choose == "1")
             {
-                Console.Write("Guess a letter: ");
-
-                input = Console.ReadLine().ToUpper();
-                guess = input[0];
-
-                if (correctGuesses.Contains(guess))
+                while (!won && lives > 0)
                 {
-                    Console.WriteLine("You've already tried '{0}', and it was correct!", guess);
-                    continue;
-                }
-                else if (incorrectGuesses.Contains(guess))
-                {
-                    Console.WriteLine("You've already tried '{0}', and it was wrong!", guess);
-                    continue;
-                }
+                    Console.Write("Guess a letter: ");
 
-                if (wordToGuessUppercase.Contains(guess))
-                {
-                    correctGuesses.Add(guess);
+                    input = Console.ReadLine().ToUpper();
+                    guess = input[0];
 
-                    for (int i = 0; i < wordToGuess.Length; i++)
+                    if (correctGuesses.Contains(guess))
                     {
-                        if (wordToGuessUppercase[i] == guess)
-                        {
-                            displayToPlayer[i] = wordToGuess[i];
-                            lettersRevealed++;
-                        }
+                        Console.WriteLine("You've already tried '{0}', and it was correct!", guess);
+                        continue;
+                    }
+                    else if (incorrectGuesses.Contains(guess))
+                    {
+                        Console.WriteLine("You've already tried '{0}', and it was wrong!", guess);
+                        continue;
                     }
 
-                    if (lettersRevealed == wordToGuess.Length)
-                        won = true;
+                    if (wordToGuessUppercase.Contains(guess))
+                    {
+                        correctGuesses.Add(guess);
+
+                        for (int i = 0; i < wordToGuess.Length; i++)
+                        {
+                            if (wordToGuessUppercase[i] == guess)
+                            {
+                                displayToPlayer[i] = wordToGuess[i];
+                                lettersRevealed++;
+                            }
+                        }
+
+                        if (lettersRevealed == wordToGuess.Length)
+                            won = true;
+                    }
+                    else
+                    {
+                        incorrectGuesses.Add(guess);
+
+                        Console.WriteLine("Nope, there's no '{0}' in it!", guess);
+                        lives--;
+                        Console.WriteLine(" guess left: " + lives + "\n");
+                    }
+
+                    Console.WriteLine(displayToPlayer.ToString());
                 }
+
+                if (won)
+                    Console.WriteLine("You won!");
                 else
-                {
-                    incorrectGuesses.Add(guess);
+                    Console.WriteLine("You lost! It was '{0}'", wordToGuess);
 
-                    Console.WriteLine("Nope, there's no '{0}' in it!", guess);
-                    lives--;
-                    Console.WriteLine(" guess left: " + lives + "\n");
-                }
-
-                Console.WriteLine(displayToPlayer.ToString());
+                Console.Write("Press ENTER to exit...");
+                Console.ReadLine();  // här under kommer för hela ordet gissning!
             }
+            else if (choose == "2") 
+            {
+                while (!won && lives > 0)
+                {
+                    Console.WriteLine("guess the word");
 
-            if (won)
-                Console.WriteLine("You won!");
-            else
-                Console.WriteLine("You lost! It was '{0}'", wordToGuess);
-            
-            Console.Write("Press ENTER to exit...");
-            Console.ReadLine();
+                    input = Console.ReadLine().ToUpper();
+                    guess = input[0];
+
+                    //if ()
+                }
+            }
         }
+            
     }
 }
